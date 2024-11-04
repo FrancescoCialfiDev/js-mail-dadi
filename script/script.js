cfecf0b9239af48613bdde34af0933c05a050e2e
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-04 15:37:41 +0100

    added ciclo for

---
+++ script/script.js
@@ -0,0 +1,34 @@
+// Applichiamo controlli ristretti del codice per una convalida piu efficiente.
+"Use strict"
+// Effettuiamo un reset automatico della console in modo tale da eliminare eventuali righe di codice sulla console.
+console.clear()
+
+// Consegna:
+/* Mail Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email */
+
+// Creiamo l'array con le varie email inserite all'interno di un database.
+let database = [ 
+"luca.bianchi123@emailfinto.com",
+"sara.rossi98@fakemail.net",
+"marco.verdi2000@tempmail.org",
+"giulia.costa89@fakeinbox.com",
+"andrea.riva77@fakemail.co",
+"elena.martini21@tempmail.me",
+"filippo.gallo55@nomail.com",
+"alessia.fontana43@discardmail.com",
+"matteo.sartori12@mytemp.email",
+"marta.pellegrini09@tempmailservice.com" ]
+
+// Creiamo una variabile di input per l'utente nel quale verrà inserita la sua email personale.
+let email = prompt("Inserisci la tua E-mail");
+console.log(database.length);
+
+// Per fare una verifica delle email di corrispondenza utilizziamo un ciclo for per scorrere all'interno dell'array
+for (let i = 0; i < array.length; i++) {
+    
+    
+}
+
+
